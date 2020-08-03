47328fc1300687a50ebab2944d45efe298323fab
endalk <endalkhbel@gmail.com>
2020-08-03 11:23:53 +0300

    middleware problem fixed

--- app/settings.py
+++ app/settings.py
@@ -57,6 +57,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = "app.urls"
 
