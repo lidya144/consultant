ef6170efed289babe441485f436b8717d33fd6fe
endalk <endalkhbel@gmail.com>
2020-08-08 10:50:21 +0300

    exam app changed

--- exam/serializers.py
+++ exam/serializers.py
@@ -3,13 +3,46 @@ from models_app.models import (
     ExamChoiceQuestionModel,
     ExamBlankSpaceQuestionModel,
     ExamDescribeQuestionModel,
-    ExamMatchQuestionModel,
+    Options,
+    MatchingInstructionModel,
+    MatchingPosibleAnswersModels,
+    MatchingQuestionModel,
 )
 
-# from subject.serializers import SubjectSerializers
+
+class MatchingPosibleAnswersSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = MatchingPosibleAnswersModels
+        fields = "__all__"
+
+
+class MatchingQuestionSerializers(serializers.ModelSerializer):
+    # question_answers = MatchingPosibleAnswersSerializers(many=True, read_only=True)
+
+    class Meta:
+        model = MatchingQuestionModel
+        fields = "__all__"
+
+
+class MatchingInstructionSerializers(serializers.ModelSerializer):
+    instruction_question = MatchingQuestionSerializers(many=True, read_only=True)
+    instruction_posible = MatchingPosibleAnswersSerializers(many=True, read_only=True)
+    question_answers = MatchingPosibleAnswersSerializers(many=True, read_only=True)
+
+    class Meta:
+        model = MatchingInstructionModel
+        fields = "__all__"
+
+
+class OptionSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Options
+        fields = "__all__"
 
 
 class ChoiceQuestionSerializers(serializers.ModelSerializer):
+    exam_option = OptionSerializers(many=True, read_only=True)
+
     class Meta:
         model = ExamChoiceQuestionModel
         fields = "__all__"
@@ -26,8 +59,3 @@ class DescribeQuestionSerializers(serializers.ModelSerializer):
         model = ExamDescribeQuestionModel
         fields = "__all__"
 
-
-class MatchQuestionSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = ExamMatchQuestionModel
-        fields = "__all__"
--- exam/views.py
+++ exam/views.py
@@ -4,14 +4,55 @@ from models_app.models import (
     ExamChoiceQuestionModel,
     ExamBlankSpaceQuestionModel,
     ExamDescribeQuestionModel,
-    ExamMatchQuestionModel,
+    Options,
+    MatchingInstructionModel,
+    MatchingPosibleAnswersModels,
+    MatchingQuestionModel,
 )
 from .serializers import (
     ChoiceQuestionSerializers,
     BlankQuestionSerializers,
     DescribeQuestionSerializers,
-    MatchQuestionSerializers,
+    OptionSerializers,
+    MatchingInstructionSerializers,
+    MatchingPosibleAnswersSerializers,
+    MatchingQuestionSerializers,
 )
+
+
+class MatchingInstructionList(generics.ListCreateAPIView):
+    queryset = MatchingInstructionModel.objects.all()
+    serializer_class = MatchingInstructionSerializers
+
+
+class MatchingQuestionList(generics.ListCreateAPIView):
+    queryset = MatchingQuestionModel.objects.all()
+    serializer_class = MatchingQuestionSerializers
+
+
+class MatchingPosibleAnswerList(generics.ListCreateAPIView):
+    queryset = MatchingPosibleAnswersModels.objects.all()
+    serializer_class = MatchingPosibleAnswersSerializers
+
+
+class OptionList(generics.ListCreateAPIView):
+    queryset = Options.objects.all()
+    serializer_class = OptionSerializers
+
+
+class OptionRUD(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Options.objects.all()
+    serializer_class = OptionSerializers
+    lookup_field = "id"
+
+    def get(self, request, id=None):
+        return self.retrieve(request, id)
+
+    def put(self, request, id=None):
+        return self.partial_update(request, id)
+
+    def delete(self, request, id=None):
+        return self.destroy(request, id)
 
 
 class ChoiceQuestionList(generics.ListCreateAPIView):
@@ -21,7 +62,7 @@ class ChoiceQuestionList(generics.ListCreateAPIView):
     # permission_classes = (permissions.IsAuthenticated,)
 
     # def get_queryset(self):
-    #     return self.queryset.filter(language=1)
+    #
 
 
 class ChoiceQuestionRUD(generics.RetrieveUpdateDestroyAPIView):
@@ -75,7 +116,7 @@ class BlankQuestionRUD(generics.RetrieveUpdateDestroyAPIView):
 
 
 class DescribeQuestionList(generics.ListCreateAPIView):
-    queryset = ExamMatchQuestionModel.objects.all()
+    queryset = ExamDescribeQuestionModel.objects.all()
     serializer_class = DescribeQuestionSerializers
 
     # permission_classes = (permissions.IsAuthenticated,)
@@ -85,7 +126,7 @@ class DescribeQuestionList(generics.ListCreateAPIView):
 
 
 class DescribeQuestionRUD(generics.RetrieveUpdateDestroyAPIView):
-    queryset = ExamMatchQuestionModel.objects.all()
+    queryset = ExamDescribeQuestionModel.objects.all()
     serializer_class = DescribeQuestionSerializers
     lookup_field = "questionId"
 
