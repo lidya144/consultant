ebb7200cf88348e255b9614b3b28f21c1321882b
endalk <endalkhbel@gmail.com>
2020-08-13 16:15:02 +0300

    configure email address

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.documentation import include_docs_urls
 from rest_framework_swagger.views import get_swagger_view
+from django.contrib.auth import views as auth_views
 
 schema_view = get_swagger_view(title="E-Learning documentation")
 
@@ -22,4 +23,22 @@ urlpatterns = [
     path(base_url + "quizes/", include("quize.urls")),
     path(base_url, include("region.urls")),
     path(base_url, include("device.urls")),
+    path(
+        "reset_password/", auth_views.PasswordResetView.as_view(), name="password_reset"
+    ),
+    path(
+        "reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset_password_sent/",
+        auth_views.PasswordResetDoneView.as_view(),
+        name="password_reset_done",
+    ),
+    path(
+        "reset_password_complete/",
+        auth_views.PasswordResetCompleteView.as_view(),
+        name="password_reset_complete",
+    ),
 ]
