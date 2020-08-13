0b34b13b43f83875480e58ba799da5b265cf6dc6
endalk <endalkhbel@gmail.com>
2020-08-13 13:34:49 +0300

    remove category apps

--- category/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import include, path
-from category import views
-urlpatterns = [
-    path("categories/",views.CategoryList.as_view()),
-    path("categories/<int:categoryId>/",views.CategoryRUD.as_view())
-    
-]
